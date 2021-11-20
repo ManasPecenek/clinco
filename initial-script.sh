ab6233ecaddfcdfaaabdd20ad5b9250a5216c75b
Manas Pecenek <61777390+ManasPecenek@users.noreply.github.com>
2021-11-20 17:02:30 +0300

    Create initial-script.sh

---
+++ initial-script.sh
@@ -0,0 +1,384 @@
+## This script prepares the overall environment
+
+docker run -dt --network macaroni --hostname master --name master -v etcd:/lib/etcd -v master:/root --ip=172.172.0.1 -p 6443:6443 -p 80:80 --privileged --user root petschenek/ubuntu-systemd
+
+
+docker run -dt --network macaroni --hostname worker --name worker -v /lib/modules:/lib/modules:ro -v worker:/root --ip=172.172.0.2 --privileged --user root petschenek/ubuntu-systemd
+
+
+cat > ca-config.json <<EOF
+{
+  "signing": {
+    "default": {
+      "expiry": "8760h"
+    },
+    "profiles": {
+      "kubernetes": {
+        "usages": ["signing", "key encipherment", "server auth", "client auth"],
+        "expiry": "8760h"
+      }
+    }
+  }
+}
+EOF
+
+cat > ca-csr.json <<EOF
+{
+  "CN": "Kubernetes",
+  "key": {
+    "algo": "rsa",
+    "size": 2048
+  },
+  "names": [
+    {
+      "C": "US",
+      "L": "Portland",
+      "O": "Kubernetes",
+      "OU": "CA",
+      "ST": "Oregon"
+    }
+  ]
+}
+EOF
+
+cfssl gencert -initca ca-csr.json | cfssljson -bare ca
+
+cat > admin-csr.json <<EOF
+{
+  "CN": "admin",
+  "key": {
+    "algo": "rsa",
+    "size": 2048
+  },
+  "names": [
+    {
+      "C": "US",
+      "L": "Portland",
+      "O": "system:masters",
+      "OU": "Kubernetes The Hard Way",
+      "ST": "Oregon"
+    }
+  ]
+}
+EOF
+
+cfssl gencert \
+  -ca=ca.pem \
+  -ca-key=ca-key.pem \
+  -config=ca-config.json \
+  -profile=kubernetes \
+  admin-csr.json | cfssljson -bare admin
+
+instance=worker
+
+cat > ${instance}-csr.json <<EOF
+{
+  "CN": "system:node:${instance}",
+  "key": {
+    "algo": "rsa",
+    "size": 2048
+  },
+  "names": [
+    {
+      "C": "US",
+      "L": "Portland",
+      "O": "system:nodes",
+      "OU": "Kubernetes The Hard Way",
+      "ST": "Oregon"
+    }
+  ]
+}
+EOF
+
+EXTERNAL_IP=172.172.0.2
+
+INTERNAL_IP=127.0.0.1
+
+cfssl gencert \
+  -ca=ca.pem \
+  -ca-key=ca-key.pem \
+  -config=ca-config.json \
+  -hostname=${instance},${EXTERNAL_IP},${INTERNAL_IP} \
+  -profile=kubernetes \
+  ${instance}-csr.json | cfssljson -bare ${instance}
+
+cat > kube-controller-manager-csr.json <<EOF
+{
+  "CN": "system:kube-controller-manager",
+  "key": {
+    "algo": "rsa",
+    "size": 2048
+  },
+  "names": [
+    {
+      "C": "US",
+      "L": "Portland",
+      "O": "system:kube-controller-manager",
+      "OU": "Kubernetes The Hard Way",
+      "ST": "Oregon"
+    }
+  ]
+}
+EOF
+
+cfssl gencert \
+  -ca=ca.pem \
+  -ca-key=ca-key.pem \
+  -config=ca-config.json \
+  -profile=kubernetes \
+  kube-controller-manager-csr.json | cfssljson -bare kube-controller-manager
+
+cat > kube-proxy-csr.json <<EOF
+{
+  "CN": "system:kube-proxy",
+  "key": {
+    "algo": "rsa",
+    "size": 2048
+  },
+  "names": [
+    {
+      "C": "US",
+      "L": "Portland",
+      "O": "system:node-proxier",
+      "OU": "Kubernetes The Hard Way",
+      "ST": "Oregon"
+    }
+  ]
+}
+EOF
+
+cfssl gencert \
+  -ca=ca.pem \
+  -ca-key=ca-key.pem \
+  -config=ca-config.json \
+  -profile=kubernetes \
+  kube-proxy-csr.json | cfssljson -bare kube-proxy
+
+cat > kube-scheduler-csr.json <<EOF
+{
+  "CN": "system:kube-scheduler",
+  "key": {
+    "algo": "rsa",
+    "size": 2048
+  },
+  "names": [
+    {
+      "C": "US",
+      "L": "Portland",
+      "O": "system:kube-scheduler",
+      "OU": "Kubernetes The Hard Way",
+      "ST": "Oregon"
+    }
+  ]
+}
+EOF
+
+cfssl gencert \
+  -ca=ca.pem \
+  -ca-key=ca-key.pem \
+  -config=ca-config.json \
+  -profile=kubernetes \
+  kube-scheduler-csr.json | cfssljson -bare kube-scheduler
+
+
+KUBERNETES_PUBLIC_ADDRESS=$(hostname)
+KUBERNETES_HOSTNAMES=kubernetes,kubernetes.default,kubernetes.default.svc,kubernetes.default.svc.cluster,kubernetes.svc.cluster.local
+
+cat > kubernetes-csr.json <<EOF
+{
+  "CN": "kubernetes",
+  "key": {
+    "algo": "rsa",
+    "size": 2048
+  },
+  "names": [
+    {
+      "C": "US",
+      "L": "Portland",
+      "O": "Kubernetes",
+      "OU": "Kubernetes The Hard Way",
+      "ST": "Oregon"
+    }
+  ]
+}
+EOF
+
+cfssl gencert \
+  -ca=ca.pem \
+  -ca-key=ca-key.pem \
+  -config=ca-config.json \
+  -hostname=10.32.0.1,172.172.0.1,172.172.0.2,${KUBERNETES_PUBLIC_ADDRESS},127.0.0.1,${KUBERNETES_HOSTNAMES} \
+  -profile=kubernetes \
+  kubernetes-csr.json | cfssljson -bare kubernetes
+
+
+cat > service-account-csr.json <<EOF
+{
+  "CN": "service-accounts",
+  "key": {
+    "algo": "rsa",
+    "size": 2048
+  },
+  "names": [
+    {
+      "C": "US",
+      "L": "Portland",
+      "O": "Kubernetes",
+      "OU": "Kubernetes The Hard Way",
+      "ST": "Oregon"
+    }
+  ]
+}
+EOF
+
+cfssl gencert \
+  -ca=ca.pem \
+  -ca-key=ca-key.pem \
+  -config=ca-config.json \
+  -profile=kubernetes \
+  service-account-csr.json | cfssljson -bare service-account
+
+docker cp ca.pem ${instance}:/root/
+docker cp ${instance}-key.pem ${instance}:/root/
+docker cp ${instance}.pem ${instance}:/root/
+
+docker cp ca.pem master:/root/
+docker cp ca-key.pem master:/root/
+docker cp kubernetes-key.pem master:/root/
+docker cp kubernetes.pem master:/root/
+docker cp service-account-key.pem master:/root/
+docker cp service-account.pem master:/root/
+
+
+
+kubectl config set-cluster kubernetes-the-hard-way \
+--certificate-authority=ca.pem \
+--embed-certs=true \
+--server=https://${KUBERNETES_PUBLIC_ADDRESS}:6443 \
+--kubeconfig=${instance}.kubeconfig
+
+kubectl config set-credentials system:node:${instance} \
+--client-certificate=${instance}.pem \
+--client-key=${instance}-key.pem \
+--embed-certs=true \
+--kubeconfig=${instance}.kubeconfig
+
+kubectl config set-context default \
+--cluster=kubernetes-the-hard-way \
+--user=system:node:${instance} \
+--kubeconfig=${instance}.kubeconfig
+
+kubectl config use-context default --kubeconfig=${instance}.kubeconfig
+
+
+kubectl config set-cluster kubernetes-the-hard-way \
+--certificate-authority=ca.pem \
+--embed-certs=true \
+--server=https://${KUBERNETES_PUBLIC_ADDRESS}:6443 \
+--kubeconfig=kube-proxy.kubeconfig
+
+kubectl config set-credentials system:kube-proxy \
+--client-certificate=kube-proxy.pem \
+--client-key=kube-proxy-key.pem \
+--embed-certs=true \
+--kubeconfig=kube-proxy.kubeconfig
+
+kubectl config set-context default \
+--cluster=kubernetes-the-hard-way \
+--user=system:kube-proxy \
+--kubeconfig=kube-proxy.kubeconfig
+
+kubectl config use-context default --kubeconfig=kube-proxy.kubeconfig
+
+
+kubectl config set-cluster kubernetes-the-hard-way \
+--certificate-authority=ca.pem \
+--embed-certs=true \
+--server=https://127.0.0.1:6443 \
+--kubeconfig=kube-controller-manager.kubeconfig
+
+kubectl config set-credentials system:kube-controller-manager \
+--client-certificate=kube-controller-manager.pem \
+--client-key=kube-controller-manager-key.pem \
+--embed-certs=true \
+--kubeconfig=kube-controller-manager.kubeconfig
+
+kubectl config set-context default \
+--cluster=kubernetes-the-hard-way \
+--user=system:kube-controller-manager \
+--kubeconfig=kube-controller-manager.kubeconfig
+
+kubectl config use-context default --kubeconfig=kube-controller-manager.kubeconfig
+
+
+kubectl config set-cluster kubernetes-the-hard-way \
+--certificate-authority=ca.pem \
+--embed-certs=true \
+--server=https://127.0.0.1:6443 \
+--kubeconfig=kube-scheduler.kubeconfig
+
+kubectl config set-credentials system:kube-scheduler \
+--client-certificate=kube-scheduler.pem \
+--client-key=kube-scheduler-key.pem \
+--embed-certs=true \
+--kubeconfig=kube-scheduler.kubeconfig
+
+kubectl config set-context default \
+--cluster=kubernetes-the-hard-way \
+--user=system:kube-scheduler \
+--kubeconfig=kube-scheduler.kubeconfig
+
+kubectl config use-context default --kubeconfig=kube-scheduler.kubeconfig
+
+
+kubectl config set-cluster kubernetes-the-hard-way \
+--certificate-authority=ca.pem \
+--embed-certs=true \
+--server=https://127.0.0.1:6443 \
+--kubeconfig=admin.kubeconfig
+
+kubectl config set-credentials admin \
+--client-certificate=admin.pem \
+--client-key=admin-key.pem \
+--embed-certs=true \
+--kubeconfig=admin.kubeconfig
+
+kubectl config set-context default \
+--cluster=kubernetes-the-hard-way \
+--user=admin \
+--kubeconfig=admin.kubeconfig
+
+kubectl config use-context default --kubeconfig=admin.kubeconfig
+
+
+docker cp ${instance}.kubeconfig ${instance}:/root/
+docker cp kube-proxy.kubeconfig ${instance}:/root/
+docker cp admin.kubeconfig master:/root/
+docker cp kube-scheduler.kubeconfig master:/root/
+docker cp kube-controller-manager.kubeconfig master:/root/
+
+
+ENCRYPTION_KEY=$(head -c 32 /dev/urandom | base64)
+
+cat > encryption-config.yaml <<EOF
+kind: EncryptionConfig
+apiVersion: v1
+resources:
+  - resources:
+      - secrets
+    providers:
+      - aescbc:
+          keys:
+            - name: key1
+              secret: ${ENCRYPTION_KEY}
+      - identity: {}
+EOF
+
+docker cp encryption-config.yaml master:/root/
+
+docker cp 07_08.sh master:/root/
+
+docker cp 09.sh worker:/root/
+
+docker exec -it --privileged --user root master bash -c "./07_08.sh"
+docker exec -it --privileged --user root worker bash -c "./09.sh"
