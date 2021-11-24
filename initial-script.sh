bba48703f1050401d375405258694e41c1a389e1
Manas Pecenek <61777390+ManasPecenek@users.noreply.github.com>
2021-11-24 19:19:55 +0300

    added /sys/fs/cgroup:/sys/fs/cgroup:ro bind mount

--- initial-script.sh
+++ initial-script.sh
@@ -1,11 +1,11 @@
 ## This script prepares the overall environment
 
-docker run -dt --network macaroni --hostname master --name master -v master:/root -v etcd:/var/lib/etcd --ip=172.172.0.1 -p 6443:6443 -p 80:80 --privileged --user root petschenek/ubuntu-systemd
+docker run -dt --network macaroni --hostname master --name master -v master:/root -v etcd:/var/lib/etcd -v /sys/fs/cgroup:/sys/fs/cgroup:ro --ip=172.172.0.1 -p 6443:6443 -p 80:80 --privileged --user root petschenek/ubuntu-systemd
 
 i=$1
 while [ $i -gt 0 ]
 do
-docker run -dt --network macaroni --hostname worker-$i --name worker-$i -v worker-$i:/root -v /lib/modules:/lib/modules:ro --ip=172.172.1.$i --privileged --user root petschenek/ubuntu-systemd
+docker run -dt --network macaroni --hostname worker-$i --name worker-$i -v worker-$i:/root -v /lib/modules:/lib/modules:ro -v /sys/fs/cgroup:/sys/fs/cgroup:ro --ip=172.172.1.$i --privileged --user root petschenek/ubuntu-systemd
 i=$((i-1))
 done
 i=$1
