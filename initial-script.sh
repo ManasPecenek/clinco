a684fcf8275176007cf37b0de95053374af1d8fb
ManasPecenek <>
2022-04-28 06:44:08 +0300

    Remove storing image volumes where we stored /root on

--- initial-script.sh
+++ initial-script.sh
@@ -22,16 +22,16 @@ elif [[ "$(uname -m)" = *"x86"* ]]
 then
   ARCH=amd64
 else
-  echo "Could not configure your archştecture" && exit 1
+  echo "Could not configure your architecture" && exit 1
 fi
 
 
-docker run -dt --network clinco --hostname master --name master -v master:/root -v etcd:/var/lib/etcd --ip=172.172.0.1 -p 6443:6443 -p 80:80 --privileged --user root petschenek/ubuntu-systemd:master-$ARCH-21.10
+docker run -dt --network clinco --hostname master --name master -v etcd:/var/lib/etcd --ip=172.172.0.1 -p 6443:6443 -p 80:80 --privileged --user root petschenek/ubuntu-systemd:master-$ARCH-21.10
 
 i=$1
 while [ $i -gt 0 ]
 do
-docker run --cpus=2 -dt --network clinco --hostname worker-$i --name worker-$i -v worker-$i:/root -v /lib/modules:/lib/modules:ro --ip=172.172.1.$i --privileged --user root petschenek/ubuntu-systemd:worker-$ARCH-21.10
+docker run --cpus=2 -dt --network clinco --hostname worker-$i --name worker-$i -v /lib/modules:/lib/modules:ro --ip=172.172.1.$i --privileged --user root petschenek/ubuntu-systemd:worker-$ARCH-21.10
 i=$((i-1))
 done
 i=$1
