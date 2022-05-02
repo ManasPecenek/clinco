79a3c557550d22e890397d1a4d2c50b3b03640b6
ManasPecenek <>
2022-05-02 12:27:31 +0300

    Add arguments to initial script

--- initial-script.sh
+++ initial-script.sh
@@ -3,12 +3,9 @@
 [[ -f "admin.kubeconfig" ]] && rm -f admin.kubeconfig
 
 
-if [ -z "$(docker network ls | grep clinco)" ]
-then
+[[ -z "$(docker network ls | grep clinco)" ]] && \
 docker network create --driver=bridge --subnet=172.172.0.0/16 --gateway=172.172.172.172 --scope=local --attachable=false --ingress=false clinco
-fi
 
-NODE_COUNT=$1
 
 if [[ "$(uname)" = *"Darwin"* ]]
 then
@@ -29,15 +26,30 @@ else
 fi
 
 
-docker run -dt --network clinco --hostname master --name master -v etcd-$RANDOM:/var/lib/etcd --ip=172.172.0.1 -p 6443:6443 -p 80:80 -p 443:443 --privileged --user root petschenek/ubuntu-systemd:master-$ARCH-21.10
+while getopts "v:n:" option; do
+  case $option in
+  v)
+    ETCD_VOLUME=$OPTARG;;
+  n) 
+    NODE_COUNT=$OPTARG;;
+  esac
+done
 
-i=$1
+
+if [[ -z "$ETCD_VOLUME" ]]
+then
+  docker run -dt --network clinco --hostname master --name master -v etcd-$RANDOM:/var/lib/etcd --ip=172.172.0.1 -p 6443:6443 -p 80:80 -p 443:443 --privileged --user root petschenek/ubuntu-systemd:master-$ARCH-21.10
+else
+  docker run -dt --network clinco --hostname master --name master -v etcd-$ETCD_VOLUME:/var/lib/etcd --ip=172.172.0.1 -p 6443:6443 -p 80:80 -p 443:443 --privileged --user root petschenek/ubuntu-systemd:master-$ARCH-21.10
+fi
+
+i=$NODE_COUNT
 while [ $i -gt 0 ]
 do
 docker run -dt --network clinco --hostname worker-$i --name worker-$i -v /lib/modules:/lib/modules:ro --ip=172.172.1.$i --privileged --user root petschenek/ubuntu-systemd:worker-$ARCH-21.10
 i=$((i-1))
 done
-i=$1
+
 
 #########################################################################################################################
 
@@ -46,24 +58,25 @@ docker exec -it --privileged --user root master bash -c "./$ARCH-master.sh $NODE
 docker cp master:/root/admin.kubeconfig .
 
 #########################################################################################################################
+j=$NODE_COUNT
 while [ $i -gt 0 ]
 do
-docker cp master:/root/worker-$i.kubeconfig .
+docker cp master:/root/worker-$j.kubeconfig .
 docker cp master:/root/kube-proxy.kubeconfig .
 docker cp master:/root/ca.pem .
-docker cp master:/root/worker-$i-key.pem .
-docker cp master:/root/worker-$i.pem .
+docker cp master:/root/worker-$j-key.pem .
+docker cp master:/root/worker-$j.pem .
 
-docker cp worker-$i.kubeconfig worker-$i:/root/ && rm -f worker-$i.kubeconfig
-docker cp kube-proxy.kubeconfig worker-$i:/root/ && rm -f kube-proxy.kubeconfig
-docker cp ca.pem worker-$i:/root/ && rm -f ca.pem
-docker cp worker-$i-key.pem worker-$i:/root/ && rm -f worker-$i-key.pem
-docker cp worker-$i.pem worker-$i:/root/ && rm -f worker-$i.pem
+docker cp worker-$j.kubeconfig worker-$j:/root/ && rm -f worker-$j.kubeconfig
+docker cp kube-proxy.kubeconfig worker-$j:/root/ && rm -f kube-proxy.kubeconfig
+docker cp ca.pem worker-$j:/root/ && rm -f ca.pem
+docker cp worker-$j-key.pem worker-$j:/root/ && rm -f worker-$j-key.pem
+docker cp worker-$j.pem worker-$j:/root/ && rm -f worker-$j.pem
 
-docker exec -it --privileged --user root worker-$i bash -c "./$ARCH-worker.sh $NODE_COUNT"
+docker exec -it --privileged --user root worker-$j bash -c "./$ARCH-worker.sh $NODE_COUNT"
 
 i=$((i-1))
 done
 #########################################################################################################################
 export KUBECONFIG=./admin.kubeconfig
-sleep 15 && kubectl apply -f kube-tools/coredns-1.9.1.yaml
+[[ -z "$ETCD_VOLUME" ]] && sleep 15 && kubectl apply -f kube-tools/coredns-1.9.1.yaml
