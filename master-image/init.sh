f43f70a6d533210ca2694d96d8751da6fa22adde
ManasPecenek <mmpecenek@gmail.com>
2024-08-31 17:12:09 +0300

    Add DOCKER_BRIDGE IP

--- master-image/init.sh
+++ master-image/init.sh
@@ -13,21 +13,23 @@ KUBERNETES_HOSTNAMES="kubernetes kubernetes.default kubernetes.default.svc kuber
 
 INTERNAL_IP=172.172.0.1
 
+DOCKER_BRIDGE=172.17.0.1
+
 mkcert -install
 
-mkcert -key-file ca-key.pem -cert-file ca.pem 127.0.0.1 10.32.0.1 ${KUBERNETES_PUBLIC_ADDRESS} ${INTERNAL_IP} ${KUBERNETES_HOSTNAMES}
+mkcert -key-file ca-key.pem -cert-file ca.pem 127.0.0.1 10.32.0.1 ${KUBERNETES_PUBLIC_ADDRESS} ${INTERNAL_IP} ${KUBERNETES_HOSTNAMES} ${DOCKER_BRIDGE}
 
-mkcert -key-file kubernetes-key.pem -cert-file kubernetes.pem 127.0.0.1 10.32.0.1 ${KUBERNETES_PUBLIC_ADDRESS} ${INTERNAL_IP} ${KUBERNETES_HOSTNAMES}
+mkcert -key-file kubernetes-key.pem -cert-file kubernetes.pem 127.0.0.1 10.32.0.1 ${KUBERNETES_PUBLIC_ADDRESS} ${INTERNAL_IP} ${KUBERNETES_HOSTNAMES} ${DOCKER_BRIDGE}
 
-mkcert -key-file service-account-key.pem -cert-file service-account.pem 127.0.0.1 10.32.0.1 ${KUBERNETES_PUBLIC_ADDRESS} ${INTERNAL_IP} ${KUBERNETES_HOSTNAMES}
+mkcert -key-file service-account-key.pem -cert-file service-account.pem 127.0.0.1 10.32.0.1 ${KUBERNETES_PUBLIC_ADDRESS} ${INTERNAL_IP} ${KUBERNETES_HOSTNAMES} ${DOCKER_BRIDGE}
 
-mkcert -key-file kube-proxy-key.pem -cert-file kube-proxy.pem 127.0.0.1 10.32.0.1 ${KUBERNETES_PUBLIC_ADDRESS} ${INTERNAL_IP} ${KUBERNETES_HOSTNAMES}
+mkcert -key-file kube-proxy-key.pem -cert-file kube-proxy.pem 127.0.0.1 10.32.0.1 ${KUBERNETES_PUBLIC_ADDRESS} ${INTERNAL_IP} ${KUBERNETES_HOSTNAMES} ${DOCKER_BRIDGE}
 
-mkcert -key-file kube-controller-manager-key.pem -cert-file kube-controller-manager.pem 127.0.0.1 10.32.0.1 ${KUBERNETES_PUBLIC_ADDRESS} ${INTERNAL_IP} ${KUBERNETES_HOSTNAMES}
+mkcert -key-file kube-controller-manager-key.pem -cert-file kube-controller-manager.pem 127.0.0.1 10.32.0.1 ${KUBERNETES_PUBLIC_ADDRESS} ${INTERNAL_IP} ${KUBERNETES_HOSTNAMES} ${DOCKER_BRIDGE}
   
-mkcert -key-file kube-scheduler-key.pem -cert-file kube-scheduler.pem 127.0.0.1 10.32.0.1 ${KUBERNETES_PUBLIC_ADDRESS} ${INTERNAL_IP} ${KUBERNETES_HOSTNAMES}
+mkcert -key-file kube-scheduler-key.pem -cert-file kube-scheduler.pem 127.0.0.1 10.32.0.1 ${KUBERNETES_PUBLIC_ADDRESS} ${INTERNAL_IP} ${KUBERNETES_HOSTNAMES} ${DOCKER_BRIDGE}
 
-mkcert -key-file admin-key.pem -cert-file admin.pem 127.0.0.1 10.32.0.1 ${KUBERNETES_PUBLIC_ADDRESS} ${INTERNAL_IP} ${KUBERNETES_HOSTNAMES}
+mkcert -key-file admin-key.pem -cert-file admin.pem 127.0.0.1 10.32.0.1 ${KUBERNETES_PUBLIC_ADDRESS} ${INTERNAL_IP} ${KUBERNETES_HOSTNAMES} ${DOCKER_BRIDGE}
 
 #########################################################################################################################
 while [ $i -gt 0 ]
@@ -40,7 +42,7 @@ INTERNAL_IP=172.172.1.$i # 127.0.0.1
 MASTER_IP=172.172.0.1
 
 
-mkcert -key-file worker-$i-key.pem -cert-file worker-$i.pem 127.0.0.1 $EXTERNAL_IP $INTERNAL_IP
+mkcert -key-file worker-$i-key.pem -cert-file worker-$i.pem 127.0.0.1 ${EXTERNAL_IP} 10.32.0.1 ${MASTER_IP} ${INTERNAL_IP} ${KUBERNETES_HOSTNAMES} ${DOCKER_BRIDGE}
 
 kubectl config set-cluster clinco-the-hard-way \
 --certificate-authority=ca.pem \
